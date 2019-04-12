233be54005c1f760efd6b0f1aeed97c7e3e24647
Timmy <Timmy@v129-22-124-246.VCLIENT.CWRU.Edu>
2019-04-12 13:08:11 -0700

    remove pyc files

--- mitopipeline/pipeline.py
+++ mitopipeline/pipeline.py
@@ -3,11 +3,12 @@ import luigi
 import subprocess
 import os
 import shutil
-STEPS = ".//steps"
+import pkgutil
+STEPS = os.path.join(os.path.dirname(__file__), 'steps')
 PIPELINE_START = "../../test/start"
 PIPELINE_STORE = ".//pipeline_output"
 SLURM_DIR = ".//pipeline_output/slurm"
-TOOLS = "./tools"
+TOOLS = "./tools/"
 
 class PipelineRunner(luigi.WrapperTask):
     _file = luigi.Parameter(default="")
@@ -20,26 +21,50 @@ class PipelineRunner(luigi.WrapperTask):
         else:
             yield GATK(id=mitopipeline.util.parse_fid(f))
             
-class SplitGap(luigi.Task):
+class ExtractMito(luigi.Task):
     id=luigi.Parameter()
     def run(self):
-        subprocess.call([STEPS + '/split_gap.sh', self.id, PIPELINE_START, PIPELINE_STORE + '/SplitGap', TOOLS])
+        subprocess.call([STEPS + '/extract_mito.sh', self.id, PIPELINE_START, PIPELINE_STORE + '/ExtractMito', TOOLS])
+    def output(self):
+        return luigi.LocalTarget(PIPELINE_STORE + '/ExtractMito/' + '{}_extract_mito'.format(self.id))
+
+class SplitGap(luigi.Task):
+    id=luigi.Parameter()
+    def require(self):
+        return ExtractMito(id=self.id)
+    def run(self):
+        subprocess.call([STEPS + '/split_gap.sh', self.id, PIPELINE_START + '/ExtractMito', PIPELINE_STORE + '/SplitGap', TOOLS])
     def output(self):
         return luigi.LocalTarget(PIPELINE_STORE + '/SplitGap/' + '{}_split_gap'.format(self.id))
-
-class RemoveNuMTs(luigi.Task):
+class Clipping(luigi.Task):
     id=luigi.Parameter()
     def require(self):
         return SplitGap(id=self.id)
     def run(self):
-        subprocess.call([STEPS + '/remove_numts_no_split_gap.sh', self.id, PIPELINE_START + '/SplitGap', PIPELINE_STORE + '/RemoveNuMTs', TOOLS])
+        subprocess.call([STEPS + '/clipping.sh', self.id, PIPELINE_START + '/SplitGap', PIPELINE_STORE + '/Clipping', TOOLS])
+    def output(self):
+        return luigi.LocalTarget(PIPELINE_STORE + '/Clipping/' + '{}_clipping'.format(self.id))
+class RemoveNuMTs(luigi.Task):
+    id=luigi.Parameter()
+    def require(self):
+        return Clipping(id=self.id)
+    def run(self):
+        subprocess.call([STEPS + '/remove_numts_no_split_gap.sh', self.id, PIPELINE_START + '/Clipping', PIPELINE_STORE + '/RemoveNuMTs', TOOLS])
     def output(self):
         return luigi.LocalTarget(PIPELINE_STORE + '/RemoveNuMTs/' + '{}_remove_numts'.format(self.id))
-class GATK(luigi.Task):
+class Downsample(luigi.Task):
     id=luigi.Parameter()
     def require(self):
         return RemoveNuMTs(id=self.id)
     def run(self):
-        subprocess.call([STEPS + '/gatk.sh', self.id, PIPELINE_START + '/RemoveNuMTs', PIPELINE_STORE + '/GATK', TOOLS])
+        subprocess.call([STEPS + '/downsample.sh', self.id, PIPELINE_START + '/RemoveNuMTs', PIPELINE_STORE + '/Downsample', TOOLS])
+    def output(self):
+        return luigi.LocalTarget(PIPELINE_STORE + '/Downsample/' + '{}_downsample'.format(self.id))
+class GATK(luigi.Task):
+    id=luigi.Parameter()
+    def require(self):
+        return Downsample(id=self.id)
+    def run(self):
+        subprocess.call([STEPS + '/gatk.sh', self.id, PIPELINE_START + '/Downsample', PIPELINE_STORE + '/GATK', TOOLS])
     def output(self):
         return luigi.LocalTarget(PIPELINE_STORE + '/GATK/' + '{}_gatk'.format(self.id))
