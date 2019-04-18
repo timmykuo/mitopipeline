936669d57ad269d57810621c35aa1f73c0832bd6
Timmy <Timmy@Timothys-MacBook-Pro.local>
2019-04-17 21:06:34 -0700

    add download links

--- doc/configuration.rst
+++ doc/configuration.rst
@@ -1,7 +1,7 @@
 Command Line Options
 ********************
 
-This section explores the different options that are possible when running the tool. 
+This section explores the different options that are possible when running the tool. Before running the tool, you can first run the command ``mitopipeline -d`` in order to download all of the dependencies necessary for the steps in your pipeline if you do not specify a tools directory. 
 
 Required Arguments
 ------------------
@@ -9,7 +9,7 @@ Required Arguments
 .. csv-table::
     :header: "Option", "Description"
 
-    "'-
+    "'-d' or '--download'", ""
 
 Optional Arguments
 ------------------
--- mitopipeline/downloader.py
+++ mitopipeline/downloader.py
@@ -1,4 +1,4 @@
-import urllib.request, pkg_resources, shutil
+import urllib.request, pkg_resources, shutil, subprocess
 from mitopipeline.util import is_downloaded
 TOOLS = pkg_resources.resource_filename('mitopipeline', "tools")
 class Downloader:
@@ -11,24 +11,42 @@ class Downloader:
                                 'clipping': ['samtools', 'bwa', 'bedtools', 'seqtk'],
                                 'extractmito': ['samtools'],
                                 'splitgap': ['samtools']}
-        self.links = {'snpeff': 'snpeff',
-                        'annovar': 'annovar',
-                        'gatk': 'gatk',
-                        'samtools': 'samtools',
-                        'bwa': 'bwa',
-                        'bedtools': 'bedtools',
-                        'seqtk': 'seqtk',
+        self.downloads = {'snpeff': 'http://sourceforge.net/projects/snpeff/files/snpEff_latest_core.zip/download',
+                        'annovar': 'None',
+                        'gatk': 'https://software.broadinstitute.org/gatk/download/auth?package=GATK-archive&version=3.1-1-g07a4bf8',
+                        'samtools': 'https://github.com/samtools/samtools/releases/download/1.9/samtools-1.9.tar.bz2',
+                        'bwa': 'git clone https://github.com/lh3/bwa.git',
+                        'bam2fastq': 'git clone --recursive https://github.com/jts/bam2fastq',
+                        'seqtk': 'git clone https://github.com/lh3/seqtk.git',
                         'rCRS': 'rCRS',
                         'hg38-nocrs': 'hg38-norcrs'}
 
-    def download_dependencies(self, steps):
+    def download_dependencies(self, steps, tools=None):
+        def is_link(software):
+            return software.startswith("https://")
+        def is_gitclone(software):
+            return software.startswith("git clone")
         for step in steps:
             for software in self.dependencies[step]:
                 if not is_downloaded(software):
                     print('Downloading software dependencies for ' + step)
-                    self.download(software, self.links[software])
-
-    def download(self, software, url):
+                    if is_link(self.downloads[software]):
+                        self.download_link(software, self.downloads[software])
+                    elif is_gitclone(self.downloads[software]):
+                        self.git_clone(software, self.downloads[software], tools)
+                    else:
+                        raise ValueError('The software annovar needs registration to be downloaded. You can register here: http://www.openbioinformatics.org/annovar/annovar_download_form.php')
+    
+    def git_clone(self, software, command, tools):
+        if tools:
+            subprocess.call(['cd', tools])
+        else:
+            subprocess.call(['cd', TOOLS])
+        subprocess.call([command])
+        subprocess.call(['cd', software])
+        subprocess.call(['make'])
+        
+    def download_link(self, software, url):
         # Download the file from `url` and save it locally under `file_name`:
         with urllib.request.urlopen(url) as response, open(str(software), 'wb') as out_file:
             shutil.copyfileobj(response, out_file)
