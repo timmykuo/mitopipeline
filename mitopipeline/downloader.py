a9c659ed614d2833a6f6e1ffa6b5894d4b687cfa
Timmy <Timmy@Timothys-MacBook-Pro.local>
2019-04-24 18:00:52 -0700

    fix bugs for downloading, making, and adding samtools to the command line

--- mitopipeline/downloader.py
+++ mitopipeline/downloader.py
@@ -1,5 +1,5 @@
 import os, platform, urllib.request, pkg_resources, shutil, subprocess, tarfile, zipfile, requests, io
-from mitopipeline.util import is_downloaded, is_exe, cd, which
+from mitopipeline.util import is_downloaded, is_exe, cd, which, execute, query_yes_no
 TOOLS = pkg_resources.resource_filename('mitopipeline', "tools")
 class Downloader:
 
@@ -36,7 +36,9 @@ class Downloader:
                     if not which(software):
                         print(software + ' is not available on the command line. Starting download...')
                         self.download(software, TOOLS)
-                        self.add_to_command_line(software, TOOLS + "/" + software)
+                        dir_name = self.get_dir_name(software, TOOLS)
+                        self.make(software, dir_name)
+                        self.add_to_command_line(software, dir_name)
                     else:
                         print(software + " is available on the command line. Skipping download.")
                 #TODO: copy from TOOLS to TOOLS + "/" + software
@@ -45,11 +47,24 @@ class Downloader:
                 else:
                     print("Downloading " + software + " to mitopipeline's tool directory")
                     self.download(software, TOOLS)
+                    self.rename_folder(software, TOOLS)
         if self.msgs:
             self.print_msgs()
         else:
             print("Downloads successful.")
     
+    def rename_folder(self, software, dir):
+        for name in os.listdir(dir):
+            if os.path.isdir(dir + "/" + name) and software in name:
+                print("got here")
+                os.rename(dir + "/" + name, dir + "/" + software)
+
+    def get_dir_name(self, software, dir):
+        for name in os.listdir(dir):
+            if os.path.isdir(dir + "/" + name) and software in name:
+                return dir + "/" + name
+        raise FileNotFoundError("Download unsuccessful. Can't find the downlaoded directory")
+
     def download(self, software, dir):
         print("Downloading " + software + ". This may take awhile...")
         url = self.downloads[software]
@@ -72,41 +87,39 @@ class Downloader:
             print("git clone unsuccessful")
             print(e.output)
             raise
-            
-        #if make install command is unavailable (samtools)
-        try:
-            subprocess.check_output(['make', 'install'])
-        except subprocess.CalledProcessError:
-            print("'make install' command unavailable. Attempting to make " + software)
+
+    def make(self, software, dir):    
+        with cd(str(dir)):
+            #if make install command is unavailable (samtools)
             try:
-                subprocess.check_output(['make'])
+                for path in execute(["make"]):
+                    print(path, end="")
             except subprocess.CalledProcessError as e:
                 print(e.output)
                 print("'make' command unavailable. Please attempt this manually through the git cloned directory at " + dir)
-                raise
+                raise e
 
     def add_to_command_line(self, software, dir):
         with cd(str(dir)):
             #move to /usr/local/bin for command line usage
-            if is_exe(dir + "/" + software + "/" + software):
-                if platform.system() == 'Darwin':
-                    print("Downloaded successfully and make successfully. Looks like you\'re using a Mac. Requesting permission to copy " + software + " executable over to /usr/local/bin...")
+            if platform.system() == 'Darwin':
+                print("Downloaded successfully and make successfully. Looks like you\'re using a Mac.")
+                if query_yes_no("Permission to copy " + software + " executable over to /usr/local/bin?"):
                     try:
                         subprocess.check_output(['sudo', 'mv', software, '/usr/local/bin'])
                     except subprocess.CalledProcessError as e:
-                        print(e.output)
+                        print(e)
                         print("There was an error moving the executable to your /usr/local/bin folder. Try doing it manually or adding the directory to your $PATH variable. The path to cloned git repository directory is " + TOOLS + "/" + software)
-                        raise 
-                elif platform.system() == 'Linux':
-                    #code for linux
-                    print('Linux')
-                elif platform.system() == 'Windows':
-                    #code for windows
-                    print('Windows')
                 else:
-                    self.cache_msg(software, "Unknown operating system, please download manually.")
+                    print("Exiting... " + software + " was downloaded into " + dir)
+            elif platform.system() == 'Linux':
+                #code for linux
+                print('Linux')
+            elif platform.system() == 'Windows':
+                #code for windows
+                print('Windows')
             else:
-                self.cache_msg(software, "Make unsuccessful")
+                self.cache_msg(software, "Unknown operating system, please download manually.")
 
     #TODO write download code for each kind of file, i.e. .zip, .tar, git clone, etc
     def download_zip(self, url):
@@ -115,9 +128,24 @@ class Downloader:
         z.extractall()
 
     def download_tar(self, url):
-        tar = tarfile.open(fileobj=urllib.request.urlopen(url), mode="r|bz2")
-        print(tar.extractall(path=os.getcwd()))
-        tar.close()
+        ftpstream = urllib.request.urlopen(url)
+
+        tmpfile = io.BytesIO()
+        while True:
+            s = ftpstream.read(16384)
+            if not s:
+                break
+
+            tmpfile.write(s)
+        ftpstream.close()
+
+        #seek back to the beginning of the temporary file.
+        tmpfile.seek(0)
+
+        tfile = tarfile.open(fileobj=tmpfile, mode="r:bz2")
+        tfile.extractall(path=os.getcwd())
+        tfile.close()
+        tmpfile.close()
 
     def cache_msg(self, software, msg):
         self.msgs.append("While downloading " + software + ", the following error occured: " + msg)
